504ff5631efc56aa2cdc4dbd4f27f1d876c73751
FherAmableRicse <amablericsefher@gmail.com>
2022-07-13 04:35:44 -0500

    Component SocialLinks jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
 function App() {
   return (
     <>
-    <NavBar /> 
-    <Home />
+      <NavBar />
+      <Home />
+      <SocialLinks />
     </>
   );
 }
