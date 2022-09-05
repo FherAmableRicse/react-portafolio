29e39a4c93d2ae9879c3e6291e35c88e6e3296af
FherAmableRicse <amablericsefher@gmail.com>
2022-09-05 00:30:52 -0500

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Acerca de mi</p>
             </div>
             <p className='text-xl mt-20'>
-               Hola, soy Fher y tengo 24 años. Bachiller en Ingenería de Sistemas Computaciones buscando mi primer trabajo como desarollador Web FrontEnd.
+               Hola, soy Fher y tengo 24 años. Bachiller en Ingenería de Sistemas Computacionales buscando mi primer trabajo como desarollador Web Frontend.
                Soy una persona perservante, que adora los retos y no me doy por vencido con facilidad cuando se trata de cumplir objetivos. Actualmente estoy aprendiendo constantemente
                tecnologías relacionadas con la programación y el desarrollo web.
                
